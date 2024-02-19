5eaa62365af94d3891e362271beec4808252bfcd
Adriana Zhelyazkova <adriana.zhelyazkova.prokopova@strypes.eu>
2024-02-19 13:14:23 +0200

    feature: add initial models creation

--- backend/analytics/models.py
+++ backend/analytics/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Machine(models.Model):
+    name = models.CharField(max_length=255)
+    os = models.CharField(max_length=255)
+    product_type = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+    
+
+class Event(models.Model):
+    name = models.CharField(max_length=255)
+    starting_point = models.CharField(max_length=255)
+    final_point = models.CharField(max_length=255)
+    duration = models.FloatField()
+
+    def __str__(self):
+        return self.name
