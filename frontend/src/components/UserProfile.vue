0b371a38bc9d8e8ad0bcf20e5a044bad41dabc97
Adriana Zhelyazkova <adriana.zhelyazkova.prokopova@strypes.eu>
2024-02-28 15:47:48 +0200

    fix: await when updating profile

--- frontend/src/components/UserProfile.vue
+++ frontend/src/components/UserProfile.vue
@@ -1,84 +1,89 @@
 <template>
-    <div class="user-profile">
-        <h2>User Profile</h2>
-        <div>
-            <form @submit.prevent="updateUserProfile" class="user-profile-form">
-                <p><strong>Email:</strong> {{ user.username }}</p>
-                <div class="form-group">
-                    <label for="first_name">First name:</label>
-                    <input type="text" v-model="user.first_name" id="first_name" required>
-                </div>
-                <div class="form-group">
-                    <label for="last_name">Last name:</label>
-                    <input type="text" v-model="user.last_name" id="last_name" required>
-                </div>
-                <button type="submit" class="save-button">Save Changes</button>
-            </form>
+  <div class="user-profile">
+    <h2>User Profile</h2>
+    <div>
+      <form @submit.prevent="updateUserProfile" class="user-profile-form">
+        <p><strong>Email:</strong> {{ user.username }}</p>
+        <div class="form-group">
+          <label for="first_name">First name:</label>
+          <input
+            type="text"
+            v-model="user.first_name"
+            id="first_name"
+            required
+          />
         </div>
+        <div class="form-group">
+          <label for="last_name">Last name:</label>
+          <input type="text" v-model="user.last_name" id="last_name" required />
+        </div>
+        <button type="submit" class="save-button">Save Changes</button>
+      </form>
     </div>
+  </div>
 </template>
   
 <script>
 export default {
-    data() {
-        return {
-            user: {},
-        };
-    },
+  data() {
+    return {
+      user: {},
+    };
+  },
 
-    async created() {
-        await this.$store.dispatch('user/fetchCurrentUser');
-        this.user = await this.$store.getters['user/getUser'];
-    },
+  async created() {
+    await this.$store.dispatch("user/fetchCurrentUser");
+    this.user = await this.$store.getters["user/getUser"];
+  },
 
-    methods: {
-        updateUserProfile() {
-            this.$store.dispatch('user/updateUserProfile', this.user);
-            this.user = this.$store.getters['user/getUser'];
-        },
+  methods: {
+    async updateUserProfile() {
+      await this.$store.dispatch("user/updateUserProfile", this.user);
+      this.user = await this.$store.getters["user/getUser"];
     },
+  },
 };
 </script>
   
 <style scoped>
 .user-profile {
-    margin: 20px;
+  margin: 20px;
 }
 
 .user-profile-form {
-    max-width: 400px;
-    margin: auto;
+  max-width: 400px;
+  margin: auto;
 }
 
 .form-group {
-    margin-bottom: 15px;
+  margin-bottom: 15px;
 }
 
 label {
-    display: block;
-    margin-bottom: 5px;
+  display: block;
+  margin-bottom: 5px;
 }
 
 input {
-    width: 100%;
-    padding: 8px;
-    box-sizing: border-box;
-    border: 1px solid #ccc;
-    border-radius: 4px;
-    outline: none;
-    margin-top: 5px;
+  width: 100%;
+  padding: 8px;
+  box-sizing: border-box;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  outline: none;
+  margin-top: 5px;
 }
 
 button.save-button {
-    background-color: #9c27b0;
-    color: #fff;
-    border: none;
-    padding: 10px 15px;
-    border-radius: 4px;
-    cursor: pointer;
+  background-color: #9c27b0;
+  color: #fff;
+  border: none;
+  padding: 10px 15px;
+  border-radius: 4px;
+  cursor: pointer;
 }
 
 button.save-button:hover {
-    background-color: #7b1fa2;
+  background-color: #7b1fa2;
 }
 </style>
